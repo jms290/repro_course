ed6ec07f9b3241e20df3f4950dee5c64203bf3dd
jms290 <jms290@cam.ac.uk>
2017-03-15 11:29:42 -0400

    move metasearch_analysis.py into untracked directory

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -2,3 +2,4 @@
 # Download our data from OpenNeuroLab   
 # get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
 Path('data_not_in_repo').mkdir()
+get_ipython().magic('mv metasearch/ data_not_in_repo/')
